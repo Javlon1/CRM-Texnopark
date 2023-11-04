1839268a5648d51175dc86d5b1fc57e2da27a26f
Javlon1 <muhamedmijotot@gmail.com>
2023-11-04 20:38:11 +0300

    CRM

--- src/Components/layout/Header/Header.jsx
+++ src/Components/layout/Header/Header.jsx
@@ -5,11 +5,11 @@ import './Header.scss'
 export default function Header() {
     return (
         <header className='header'>
-            <div className='header__container'>
-                <div className="header__container__left">
+            <div className='container'>
+                <div className="container__left">
                     <h1>left</h1>
                 </div>
-                <div className="header__container__right">
+                <div className="container__right">
                     <h1>right</h1>
                 </div>
             </div>
