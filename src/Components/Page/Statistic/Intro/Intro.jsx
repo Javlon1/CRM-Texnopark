be446dd2621c10206aac254792d83d2407df36ca
Javlon1 <muhamedmijotot@gmail.com>
2023-11-20 21:34:51 +0300

    responsive

--- src/Components/Page/Statistic/Intro/Intro.jsx
+++ src/Components/Page/Statistic/Intro/Intro.jsx
@@ -25,7 +25,7 @@ export default function Intro() {
             </text>
         );
     };
-    
+
     const danChange = (e) => {
         setDan(e.target.value)
     };
@@ -40,66 +40,69 @@ export default function Intro() {
             <div className="intro__left"></div>
             <div className="intro__right">
                 <div className="intro__right__container">
-                    {
-                        chartsData?.map((e, i) => (
-                            <div key={i} className="intro__right__container__items">
-                                <h2 className='intro__right__container__items__title'>{e[`title_${lan}`]}</h2>
-                                <p className='intro__right__container__items__text'>{e[`text_${lan}`]}</p>
-                                <div className='intro__right__container__items__date'>
-                                    <div className='select'>
-                                        <select name="" id="" defaultValue={dan} onChange={danChange}>
-                                            {
-                                                e.dan?.map((j)=>(
-                                                    <option  key={j.id} value={j.date}>{j.date}</option>
-                                                ))
-                                            }
-                                        </select>
-                                    </div>
+                    <div className='jcsb'>
 
-                                    <div className='select'>
-                                        <select name="" id="" defaultValue={gacha} onChange={gachaChange}>
-                                            {
-                                                e.gacha?.map((j)=>(
-                                                    <option  key={j.id} value={j.date}>{j.date}</option>
-                                                ))
-                                            }
-                                        </select>
+                        {
+                            chartsData?.map((e, i) => (
+                                <div key={i} className="intro__right__container__items">
+                                    <h2 className='intro__right__container__items__title'>{e[`title_${lan}`]}</h2>
+                                    <p className='intro__right__container__items__text'>{e[`text_${lan}`]}</p>
+                                    <div className='intro__right__container__items__date'>
+                                        <div className='select'>
+                                            <select name="" id="" defaultValue={dan} onChange={danChange}>
+                                                {
+                                                    e.dan?.map((j) => (
+                                                        <option key={j.id} value={j.date}>{j.date}</option>
+                                                    ))
+                                                }
+                                            </select>
+                                        </div>
+
+                                        <div className='select'>
+                                            <select name="" id="" defaultValue={gacha} onChange={gachaChange}>
+                                                {
+                                                    e.gacha?.map((j) => (
+                                                        <option key={j.id} value={j.date}>{j.date}</option>
+                                                    ))
+                                                }
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className='intro__right__container__items__chart'>
-                                    <PieChart className='piechart' width={200} height={200}>
-                                        <Pie
-                                            className='pie'
-                                            data={e.product}
-                                            cx="50%"
-                                            cy="50%"
-                                            labelLine={false}
-                                            label={renderCustomizedLabel}
-                                            outerRadius={100}
-                                        >
+                                    <div className='intro__right__container__items__chart'>
+                                        <PieChart className='piechart' width={200} height={200}>
+                                            <Pie
+                                                className='pie'
+                                                data={e.product}
+                                                cx="50%"
+                                                cy="50%"
+                                                labelLine={false}
+                                                label={renderCustomizedLabel}
+                                                outerRadius={100}
+                                            >
+                                                {
+                                                    e.product?.map((e, i) => (
+                                                        <Cell key={`cell-${i}`} fill={e.color} />
+                                                    ))
+                                                }
+                                            </Pie>
+                                        </PieChart>
+                                        <ul className='list'>
                                             {
                                                 e.product?.map((e, i) => (
-                                                    <Cell key={`cell-${i}`} fill={e.color} />
+                                                    <li className='list__item' key={i}>
+                                                        <p style={{ backgroundColor: e.color }}></p>
+                                                        <span className='list__item__text'>
+                                                            {e[`name_${lan}`]}
+                                                        </span>
+                                                    </li>
                                                 ))
                                             }
-                                        </Pie>
-                                    </PieChart>
-                                    <ul className='list'>
-                                        {
-                                            e.product?.map((e, i) => (
-                                                <li className='list__item' key={i}>
-                                                    <p style={{ backgroundColor: e.color }}></p>
-                                                    <span className='list__item__text'>
-                                                        {e[`name_${lan}`]}
-                                                    </span>
-                                                </li>
-                                            ))
-                                        }
-                                    </ul>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </section >
